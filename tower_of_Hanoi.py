b29be1ebca20303ad9b7be83451e29cdbe81b520
Hackbright Student <user@chromebox-002.(none)>
2013-10-04 12:50:21 -0700

    Working Tower

--- tower_of_Hanoi.py
+++ tower_of_Hanoi.py
@@ -1,27 +1,30 @@
 
 #towers of Hanoi
+t1 = [5,4,3,2,1]
+t2 = []
+t3 = []
 
-def move_rings(n, start, end, temp):
+
+def move_rings(n, start, target, temp):
     # n is number of rings
     # start is starting tower
-    #end is ending tower
-    #temp is the third (middle) tower
+    #target is ending tower
+    #temp is the buffer
 
-    if n == 0:
-        return []
-    start = range(n)
-    start.reverse()
-
-    biggest = start.pop(0)
-    end.append(biggest)
-   # move_rings(n-1, start, temp, end)
-
-    print "start",start
-    print "end", end
-    print "temp",temp
-    
-
-    return move_rings(n-1, temp, end, start)
+    if n == 1:
+        target.append(start.pop())
+    else:
+        move_rings(n-1, start,temp, target)
+        target.append(start.pop())
+        move_rings(n -1,temp,target,start)
 
 
-move_rings(5, [],[],[])
+
+def print_towers():
+    print t1
+    print t2
+    print t3
+
+
+move_rings(5, t1, t2, t3)
+print_towers()
